b80d35f2142582de47a3c32966b5e9d51eb4c836
asish012 <asish012@gmail.com>
2014-11-12 22:19:22 +0100

    reading word to check from user through console

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,14 @@ int main() {
 	cout << "scheck version 0.1" << endl;
 
 	Dictionary dictionary("data/mydictionary.dat");
-	string word = "cat";
-	if(dictionary.check(word)) {
-		cout << word << " is OK\n";
-	} else {
-		cout << word << " is misspelt\n";
+	
+	string word;
+	while(getline(cin, word)) {
+		if(dictionary.check(word)) {
+			cout << word << " is OK\n";
+		} else {
+			cout << word << " is misspelt\n";
+		}
 	}
+
 }
