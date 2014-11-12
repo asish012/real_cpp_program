a0aec21ef5583634d61a1b232a1456db0cf0630a
asish012 <asish012@gmail.com>
2014-11-12 22:06:45 +0100

    word found ;)

--- inc/dictionary.h
+++ inc/dictionary.h
@@ -1,10 +1,24 @@
+#ifndef INC_DICTIONARY_H
+#define INC_DICTIONARY_H
+
 #include <string>
+#include <set>
 
 class Dictionary
 {
 public:
-	Dictionary(const std::string & filename);
-	~Dictionary();
+	Dictionary(const std::string & filename) {
+		words_.insert("dog");
+	}
 
-	bool check(const std::string & word) const;
+	~Dictionary() {}
+
+	bool check(const std::string & word) const {
+		return words_.find(word) != words_.end();
+	}
+
+private:
+	std::set <std::string> words_;
 };
+
+#endif //INC_DICTIONARY_H
